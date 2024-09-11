3748972741b059eb7a3f5ba60b45b84c1239af5c
clemsytoff <80209469+clemsytoff@users.noreply.github.com>
2024-09-11 10:05:12 +0200

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,29 @@
+class Soldat:
+    def __init__():
+        self.point_de_vie = 200
+        self.point_de_degats = 20
+        self.point_endurance = 12
+
+#ce init sert à modifier pour plus tard
+    def __init__(point_de_vie,point_de_degats,point_endurance): 
+        self.point_de_vie = 200
+        self.point_de_degats = 20
+        self.point_endurance = 12
+    
+    def bombe():
+        print("Djebz est dans la place")
+    
+    def kitdesoins():
+        self.point_de_vie=200
+    
+    def deplacements(touche_clavier):
+        if (deplacements.direction == "haut"):
+            self.y = y + 10
+        elif (deplacements.direction == "bas"):
+            self.y = y - 10
+        elif (deplacements.direction == "droite"):
+            self.y = x + 10
+        elif (deplacements.direction == "gauche"):
+            self.y = x - 10
+
+m1 = mage()
