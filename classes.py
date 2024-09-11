04bde376bdffd7ef012e5b6843d123c36b8b8e5d
clemsytoff <80209469+clemsytoff@users.noreply.github.com>
2024-09-11 10:12:53 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,19 +1,19 @@
 class Soldat:
-    def __init__():
+    def __init__(self):
         self.point_de_vie = 200
         self.point_de_degats = 20
         self.point_endurance = 12
 
 #ce init sert à modifier pour plus tard
-    def __init__(point_de_vie,point_de_degats,point_endurance): 
+    def __init__(self,point_de_vie,point_de_degats,point_endurance): 
         self.point_de_vie = 200
         self.point_de_degats = 20
         self.point_endurance = 12
     
-    def bombe():
+    def bombe(self):
         print("Djebz est dans la place")
     
-    def kitdesoins():
+    def kitdesoins(self):
         self.point_de_vie=200
     
     def deplacements(touche_clavier):
@@ -26,4 +26,5 @@ class Soldat:
         elif (deplacements.direction == "gauche"):
             self.y = x - 10
 
-m1 = mage()
+m1 = Soldat(40,50,11)
+m1.bombe()
