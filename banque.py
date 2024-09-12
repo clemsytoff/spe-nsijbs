654498c9139002341ae58936f1aebcc41771bb53
clemsytoff <80209469+clemsytoff@users.noreply.github.com>
2024-09-12 11:46:50 +0200

    Update banque.py

--- banque.py
+++ banque.py
@@ -15,8 +15,10 @@ class Compte:
             self.solde = self.solde - retrait
     
     def afficher(self):
-        if self.nom == "Elbarto":
+        if self.nom == "Elbarto" or "Bharat":
             print("Bharat tu es un clodo.")
+        elif self.nom == "djebz" or "lamenace.exe" or "lamenace" or "Djebril":
+            print("Djebz, voler n'est pas bon.")
         else:    
             print("Vous disposez de " + str(self.solde) + " € sur le compte de Mr. " + self.nom)
 
