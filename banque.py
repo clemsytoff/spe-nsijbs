21ed0deb6b25fc55cd41849edc554e912c99ea8d
clemsytoff <80209469+clemsytoff@users.noreply.github.com>
2024-09-12 11:38:02 +0200

    Create banque.py

---
+++ banque.py
@@ -0,0 +1,22 @@
+# classe pour un compte banquaire - nom titulaire et solde, 3 méthodes: déposer, retirer, afficher
+
+class Compte:
+    def __init__(self,nom="Clément",solde=1000):
+        self.nom = nom
+        self.solde = solde
+        
+    def depot(self,depot):
+        self.solde = self.solde + depot
+    
+    def retrait(self, retrait):
+        if self.solde - retrait <= 0:
+            print("Vous n'avez pas assez d'argent")
+        else:
+            self.solde = self.solde - retrait
+    
+    def afficher(self):
+        print("Vous disposez de " + str(self.solde) + " € sur le compte de Mr. " + self.nom)
+
+m1=Compte()
+m2=m1.retrait(120)
+m3=m1.afficher()
