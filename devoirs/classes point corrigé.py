2ec754e0a346f66cbe26e232c698829b6296d9e2
clemsytoff <80209469+clemsytoff@users.noreply.github.com>
2024-09-12 11:04:01 +0200

    Create classes point corrigé.py

---
+++ devoirs/classes point corrigé.py
@@ -0,0 +1,19 @@
+from math import *
+
+class Point:
+    def __init__(self,x=3,y=10):
+        self.x = x
+        self.y = y
+    
+    def translation(self, vx, vy):
+        self.x += vx
+        self.y +=vy
+    
+    def distance(self, p):
+        d = sqrt(pow(p.x - self.x,2) + pow(p.y - self.y,2))
+        return d
+
+p1=Point()
+p2 = Point(45,78)
+d = p1.distance(p2)
+print(d)
