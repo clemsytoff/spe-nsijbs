f7da78d9fff0acb85c84ee46884ec1d74802330f
clemsytoff <80209469+clemsytoff@users.noreply.github.com>
2024-09-12 07:46:20 +0200

    Update classes points.py

--- devoirs/classes points.py
+++ devoirs/classes points.py
@@ -38,7 +38,7 @@ class Point:
         
         #resultat
         self.resultatxy = self.resulty + self.resultx + 100 #on ajoute le 0 vu qu'on bouge en 10 par 10
-        self.resultatxy = (self.resultatx)^2 + (self.resultaty)^2
+        self.resultatxy = Pow(self.resultatx, 2) + Pow(self.resultaty,2)
 
 
         print("La distance entre le joueur et l'objectif est de " + str(self.resultatxy) + " pixels.")
